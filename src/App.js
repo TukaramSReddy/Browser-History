4fcab85b67f7835ca5141957a1e8d86d5a285645
Tukaram S Reddy <tukaramreddy90@gmail.com>
2023-04-07 06:19:31 +0000

    Browser History Solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import HistorySearch from './components/HistorySearch/index'
+
+import './App.css'
+
+// These are the list used in the application. You can move them to any component needed.
+
+const App = () => <HistorySearch />
+
+export default App
